1ec75da032ffe889ee62229d3ac177279146747e
Cyrus Huang <archman@126.com>
2025-04-21 15:58:52 +0800

    update doc fetcher

--- doc-fetcher/doc-fetch.sh
+++ doc-fetcher/doc-fetch.sh
@@ -4,7 +4,9 @@
 REPO_URL="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 DIR_PATH="Documentation"     # 改成你想要下载的目录，比如 kernel, fs, etc.
 BRANCH="master"   # torvalds 的分支名是 master
-CLONE_DIR="docs"
+CLONE_DIR="/var/tmp/kernel-docs"
+
+ln -s $CLONE_DIR ./docs
 
 # 克隆仓库（使用稀疏模式）
 git clone --filter=blob:none --no-checkout --depth=1 "$REPO_URL" "$CLONE_DIR"
@@ -15,4 +17,4 @@ git sparse-checkout init --cone
 git sparse-checkout set "$DIR_PATH"
 git checkout "$BRANCH"
 
-echo "✅ 目录 $DIR_PATH 已下载到 $(pwd)/$DIR_PATH"
+echo "Done (./docs => $CLONE_DIR)"
